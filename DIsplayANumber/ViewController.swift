ecf73bb33bde0da09e3a48c88792328faecdb3c2
JOHN SANCHEZ <rl200295@rlas-macair-29.local>
2018-09-25 11:01:04 -0500

    Made a button and put label

--- DIsplayANumber/ViewController.swift
+++ DIsplayANumber/ViewController.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var numberLabel: UILabel!
+    
+    @IBOutlet weak var numberTextField: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onButtonTap(_ sender: Any) {
+    let numberToDisplay = numberTextField.text
+    numberLabel.text = numberToDisplay
+    
     }
+    
+    
+    
+    
+    
+    
 
 
 }
